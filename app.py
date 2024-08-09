aa4b964acd37f9c3db1aca3506eb2bb6f22fd0db
전성일 <jeonjerry0709@gmail.com>
2024-08-09 21:43:31 +0900

    feat: add name, nickname in register

--- app.py
+++ app.py
@@ -6,47 +6,41 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 
 
-from botocore.exceptions import ClientError
-
 from werkzeug.security import generate_password_hash, check_password_hash#hashing password library
 
-from models import db, User
+from models import db, Users
 
 
 app = Flask(__name__)
 
-#DynamoDB configurations
-dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')
-table_name = 'eatprint-posts'#table name in dynamodb already created
-table = dynamodb.Table(table_name)
-
-
-# MySQL configurations
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
 db_name = os.getenv("DB_NAME")
 db_host = os.getenv("DB_HOST")
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:5432/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
 migrate = Migrate(app, db)
+#Posting section
+# @app.route('/post', methods=['POST'])
+# def post():
 
-
-#MySQL
+#User Registration and Login
 #===========================================================================================
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
     userid = data['userid']
     userpw = generate_password_hash(data['userpw'], method='pbkdf2:sha256')
+    name = data['name'] 
+    nick_name = data['nickname']
     #get the user data from the request for check it exists or not
-    existing_user = User.query.filter_by(userid=userid).first()
+    existing_user = Users.query.filter_by(user_id=userid).first()
     #check duplicate user id
     if existing_user:
         return jsonify({'message': 'User ID already exists'}), 409
     #create new user
-    new_user = User(userid=userid, userpw=userpw)
+    new_user = Users(user_id=userid, user_pw=userpw,name=name,nick_name=nick_name)
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'message': 'User registered successfully'}), 201
@@ -58,61 +52,22 @@ def login():
     userid = data['userid']
     userpw = data['userpw']
     #check user id exists
-    user = User.query.filter_by(userid=userid).first()
+    user = Users.query.filter_by(user_id=userid).first()
     #if user id does not exist
     if user is None:
         return jsonify({'message': 'User ID does not exist'}), 404
     #check password when user id exists
-    if not check_password_hash(user.userpw, userpw):
+    if not check_password_hash(user.user_pw, userpw):
         return jsonify({'message': 'Incorrect password'}), 401
     
-    return jsonify({'id': user.id, 'message': 'Login successful'}), 200
+    return jsonify({'id': user.user_num, 'message': 'Login successful'}), 200
 
 
-#dynamoDB
-@app.route('/upload', methods=['POST'])
-def upload_item():
-    data = request.json
-    usernum = data.get('usernum')
-    text = data.get('text')
-    image_data = data.get('image')  # base64로 인코딩된 이미지 데이터
 
-    if not all([usernum, text]):
-        return jsonify({'error': 'Missing usernum, text'}), 400
-
-    try:
-        # 이미지를 S3에 저장하는 로직 추가 가능
-        table.put_item(
-            Item={
-                'usernum': usernum,
-                'text': text,
-                'image': image_data  # 이미지를 base64 인코딩된 문자열로 저장
-            }
-        )
-        return jsonify({'message': 'Item created successfully'}), 201
-    except ClientError as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/items', methods=['GET'])
-def get_items():
-    try:
-        response = table.scan()
-        items = response.get('Items', [])
-        return jsonify(items)
-    except ClientError as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/item', methods=['POST'])
-def create_item():
-    item = request.json
-    try:
-        table.put_item(Item=item)
-        return jsonify({'message': 'Item created successfully'}), 201
-    except ClientError as e:
-        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        
     app.run(debug=True)
 
--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from geoalchemy2 import Geography, Geometry
+from geoalchemy2 import Geography
 
 
 
 db = SQLAlchemy()
 
 class Users(db.Model):
-    __tablename__ = 'USERS_TB'
+    __tablename__ = 'user_tb'
 
     user_num = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.String(100), unique=True, nullable=False)
@@ -15,29 +15,31 @@ class Users(db.Model):
     name = db.Column(db.String(100), nullable=False)
     nick_name = db.Column(db.String(100), nullable=False)
 
-    posts = db.relationship('Post', back_populates='USERS_TB')
+    posts = db.relationship('Post', back_populates='user')
 
 
 class Post(db.Model):
-    __tablename__ = 'POST_TB'
+    __tablename__ = 'post_tb'
     
     post_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_num = db.Column(db.Integer, db.ForeignKey('USERS_TB.user_num'))
+    user_num = db.Column(db.Integer, db.ForeignKey('user_tb.user_num'))
+    image = db.Column(db.Text)
     content = db.Column(db.Text)
     location = db.Column(Geography(geometry_type='POINT', srid=4326))
     
-    user = db.relationship('Users', back_populates='POST_TB')
+    user = db.relationship('Users', back_populates='posts')
+    hashtags = db.relationship('Hashtag', secondary='post_hashtag_tb', back_populates='posts')
 
 class Hashtag(db.Model):
-    __tablename__ = 'HASHTAG_TB'
+    __tablename__ = 'hashtag_tb'
     
     tag_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tag_name = db.Column(db.String(50), nullable=False, unique=True)
     
-    posts = db.relationship('Post', secondary='POST_HASHTAG_TB', back_populates='hashtags')
+    posts = db.relationship('Post', secondary='post_hashtag_tb', back_populates='hashtags')
 
 class PostHashtag(db.Model):
-    __tablename__ = 'POST_HASHTAG_TB'
+    __tablename__ = 'post_hashtag_tb'
     
-    post_id = db.Column(db.Integer, db.ForeignKey('POST_TB.post_id'), primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('HASHTAG_TB.tag_id'), primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post_tb.post_id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('hashtag_tb.tag_id'), primary_key=True)
